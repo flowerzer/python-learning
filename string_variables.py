8b23d8ce92dc94b5690a0cbb14d9d73857c41482
flowerzer <100711113+flowerzer@users.noreply.github.com>
2022-04-22 14:55:08 +0100

    Update and rename string_variables to string_variables.py

--- string_variables.py
+++ string_variables.py
@@ -12,3 +12,22 @@ pens_in_purse = "There are %s in my purse"
 
 print(pens_in_purse % pens )
 #output: There are 13 pens in my purse
+
+
+# Assignment
+# Write 10 sentences that have numbers in them.
+# Exmaple: 
+# "Sheila says she is 12 years old"
+# "My best friend has 3 dogs"
+
+# 1. I have 34 slippers at home.
+# 2. p
+# 3. 
+# 4. 
+# 5. 
+# 6. 
+# 7. 
+# 8.
+# 9.
+# 10.
+
