bb513f5e6fdcf26ecf85c0808e5d7f8ec74d2a21
KENDRA <kendra@KENDRAs-MacBook-Air.local>
2022-05-13 16:51:12 +0100

    assignment file created with rules

---
+++ assignments.py
@@ -0,0 +1,34 @@
+"""
+CURRENT LPS: 30
+
+GIFT REWARD: You will receive a present when you have 500 Learning Points (500LPS) 
+SUBMISSION REWARD: You gain 20LPs when you do your assignment and submit it in the next class.
+PRACTICE REWARD: You gain an extra 15LPS when you do more practice of what you learnt at home.
+LATE SUBMISSION PENALTY: You gain only 5LPS when you submit a late assignment after the next class.
+NO SUBMISSION PENALTY: You will lose 10LPS when you don't submit your assignment.
+EMAIL POINTS: You gain an extra 10LPs if you send an email of your answers to: anthonia@nugitech.com before the next class.
+
+Here is an email template you can use:
+
+Good day Miss Tonia,
+I hope you are fine, Here are the answers to my assignment:
+
+ ``` Paste your answers here ```
+ 
+"""
+
+
+""" Assignment 9 - Friday, 13th May, 2022 
+
+1. Use the first letter of each item in the list to unpack the following lists into multiple variables
+- drinks
+- clothes
+
+Tip: Look at the variables.py file for examples. """
+
+drinks = ["zobo", "sprite", "malt", "milk"]
+# type your answer below this line
+
+clothes = ["trouser", "sock", "jacket", "blouse"]
+# type your answer below this line
+
